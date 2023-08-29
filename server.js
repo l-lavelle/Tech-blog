da678bbab99e735fff2c62f52f89ea1b51dee866
Lauren <lauren.m.lavelle@wmich.edu>
2023-08-29 13:00:21 -0400

    updating server file

---
+++ server.js
@@ -0,0 +1,25 @@
+// Import dependencies 
+const express = require('express');
+const exphbs= require('express-handlebars');
+// const path = require('path');
+const routes = require('./controllers')
+const sequelize = require('./config/connection')
+const hbs = exphbs.create({});
+
+// Set up express app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set Handlebars as default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine','handlebars');
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(routes)
+
+// Starts the server to begin listening
+sequelize.sync().then(()=>{
+    app.listen(PORT, () => console.log('Server listening on: http://localhost:' + PORT));
+});
