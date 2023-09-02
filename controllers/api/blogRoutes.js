5c97a001dad0ca9e3cdca5aa7bf6da8b60732e42
Lauren <lauren.m.lavelle@wmich.edu>
2023-09-02 12:14:50 -0400

    added dependencies to node modules and set up session requirements in server.js

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
-const Blog = require('../../models/Blog');
-router.get('/', (req, res) => res.send('API !'));
+const {Blog, User} = require('../../models');
+
+router.get('/:user_id', async (req, res) => {
+    try{
+        const userPostData = await Blog.findAll({
+            include: [{ model: User }],
+            where:{
+                user_id: req.params.user_id
+            }
+        })
+        const userPosts = userPostData.map((posts)=>
+        posts.get({plain:true})
+        )
+        res.render('dashboard', {userPosts});
+    }catch (err){
+
+    }
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Import dependencies 
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs= require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -8,15 +9,35 @@ models = require('./models');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({helpers});
 
+// Initializes Sequelize with session store
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 // Set up express app
 const app = express();
 const PORT = process.env.PORT || 3002;
+
+// Sets up session and connect to Sequelize db
+const sess = {
+    secret:process.env.SESSION_SECRET,
+    cookie:{
+        maxAge: 60 * 60 * 1000,
+        httpOnly: true,
+        secure: false,
+        sameSite:'strict'
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 // Set Handlebars as default template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine','handlebars');
 
 // Middleware
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
