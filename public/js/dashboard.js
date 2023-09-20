b0afff6a65817ec7d9f361151a0160a5a7414da2
Lauren <lauren.m.lavelle@wmich.edu>
2023-09-20 18:22:50 -0400

    dashboard modal has post content

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,7 +1,27 @@
 const blogTitles = document.querySelectorAll(".blogTitle");
-blogTitles.forEach((title) => title.addEventListener("click", blogOptions));
+blogTitles.forEach((title) => title.addEventListener("click", optionsModal));
+const modal = document.getElementById("dash-modal");
 //trail for modal pop up
+async function optionsModal() {
+  modal.classList.add("is-active");
 
+  let parent = this.parentNode;
+  let textP = parent.querySelector(".blogText");
+  let titleP = parent.querySelector(".blogTitle");
+  const title = titleP.innerHTML;
+  let blogText = textP.innerHTML;
+
+  const modalBody = document.querySelector("#post-body");
+  modalBody.value = blogText;
+
+  const modalTitle = document.querySelector(".modal-card-title");
+  modalTitle.innerHTML = title;
+}
+
+async function cancelModal() {
+  console.log(3);
+  modal.classList.remove("is-active");
+}
 //will change to text area
 // async function blogOptions() {
 //   let parent = this.parentNode;
