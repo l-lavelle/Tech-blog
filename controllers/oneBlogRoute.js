db6b3068d99eab41becdb33616890abb358b1324
Lauren <lauren.m.lavelle@wmich.edu>
2023-09-20 22:15:47 -0400

    added comments to blog posts

--- controllers/oneBlogRoute.js
+++ controllers/oneBlogRoute.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
-const { Blog, Comment } = require("../models");
+const { Blog, Comment, User } = require("../models");
 
 router.get("/:blog_id", async (req, res) => {
   try {
     const blogPost = await Blog.findByPk(req.params.blog_id, {
-      include: [{ model: Comment }],
+      include: [{ model: Comment }, { model: User }],
     });
 
     const blog = blogPost.get({ plain: true });
-
+    // res.json({ blog });
     res.render("singleBlog", {
       blog,
       loggedIn: req.session.loggedIn,
